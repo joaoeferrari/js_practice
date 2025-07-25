e2438a9b19124410d45cc5af96107f44a2faa218
João Ferrari <109560169+joaoeferrari@users.noreply.github.com>
2025-07-25 14:23:11 -0300

    add 5 exercises

---
+++ basic.js
@@ -0,0 +1,56 @@
+
+// 1 Declare a variable to store your name and print it
+console.log('exercise 1');
+let name = 'joao'
+console.log(`${name} \n`);
+
+// 2 Create two number variables and print their sum, difference, product, and quotient
+
+a = 10
+b = 20
+
+console.log('exercise 2');
+console.log(`sum: ${a+b}`)
+console.log(`sub: ${a-b}`)
+console.log(`mul: ${a*b}`)
+console.log(`div: ${a/b} \n`)
+
+// 3 Swap the values of two variables without using a third variable
+
+a = 1
+b = 2
+
+a = a + b
+b = a - b
+
+console.log('exercise 3');
+console.log(`a: ${a}`)
+console.log(`b: ${b} \n`)
+
+// 4 Check the type of different variables (string, number, boolean, null, undefined)
+
+a = 5
+b = 'joao'
+c = true
+
+console.log('exercise 4');
+console.log(`a: ${typeof a}`)
+console.log(`b: ${typeof b}`)  
+console.log(`c: ${typeof c} \n`)
+
+
+// 5 Check if a number is even or odd
+
+n = 10
+
+if (n % 2 === 0) {
+    console.log('exercise 5');
+    console.log(`${n} is even \n`);
+}
+else {
+    console.log('exercise 5');
+    console.log(`${n} is odd \n`);
+}
+
+
+
